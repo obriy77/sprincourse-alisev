9d6a2520f3e62f47bbc63bf77fd421824e47b195
obriy77 <obriy77@gmail.com>
2019-02-24 16:22:09 +0300

    No work project stringcourse alishev

---
+++ src/main/java/ua/obriy77/springcourse/TestSpring.java
@@ -0,0 +1,15 @@
+package ua.obriy77.springcourse;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext(
+				"applicationContext.xml"); 
+		TestBean testBean=context.getBean("TestBean", TestBean.class);
+		
+		System.out.println(testBean.getName());
+		context.close();
+	}
+
+}
